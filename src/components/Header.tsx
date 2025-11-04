6fef11463170911117f00d0612ef3f5e898b2f0f
cezene <cezenesn@hotmail.com.br>
2025-11-04 17:48:12 -0300

    Adds Header and basic structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="/zuchetti-logo.png"
+        alt="Zucchetti"
+        style={{ height: "40px" }}
+      />
+    </header>
+  );
+};
+
+export default Header;
